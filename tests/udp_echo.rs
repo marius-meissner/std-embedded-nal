2a40be5e3ea9a599fefb583ed8dfe677e3ac9f12
chrysn <chrysn@fsfe.org>
2021-02-02 15:49:00 +0100

    Add echo test

---
+++ tests/udp_echo.rs
@@ -0,0 +1,32 @@
+use embedded_nal::nb::block;
+
+fn echo(stack: &impl embedded_nal::UdpServer, addr: &str) {
+    let addr: embedded_nal::SocketAddr = addr.parse().unwrap();
+    let mut servsock = stack.socket().unwrap();
+    let mut clisock = stack.socket().unwrap();
+
+    stack.bind(&mut servsock, addr.port()).unwrap();
+    stack.connect(&mut clisock, addr).unwrap();
+
+    block!(stack.send(&mut clisock, b"ping")).unwrap();
+    let mut buffer = [0u8; 10];
+    let (received, cliaddr) = block!(stack.receive(&mut servsock, &mut buffer)).unwrap();
+    assert_eq!(received, 4);
+    assert_eq!(&buffer[..4], b"ping");
+
+    block!(stack.send_to(&mut servsock, cliaddr, b"pong")).unwrap();
+    let mut buffer = [0u8; 10];
+    let (received, _) = block!(stack.receive(&mut clisock, &mut buffer)).unwrap();
+    assert_eq!(received, 4);
+    assert_eq!(&buffer[..4], b"pong");
+}
+
+#[test]
+fn std_echov4() {
+    echo(&std_embedded_nal::STACK, "127.0.0.1:2342");
+}
+
+#[test]
+fn std_echov6() {
+    echo(&std_embedded_nal::STACK, "[::1]:4223");
+}
